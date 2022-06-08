6e2dc039d9279cb1340229f51536a555dbf465d2
fullStackGruppe11 <237187@usn.no>
2022-06-08 11:25:28 +0200

    eksataoppgave8a: Lag en ny side for Observasjoner

---
+++ Data/Observasjon.cs
@@ -0,0 +1,12 @@
+﻿#nullable disable
+
+namespace Gruppe11.Data
+{
+    public class Observasjon
+    {
+        public int Id { get; set; }
+        public DateTime? Dato { get; set; }
+        public int? Temperatur { get; set; }
+        public string Bruker { get; set; }
+    }
+}
